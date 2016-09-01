f06976e8ebc8a062a561f98755d71c79560dedba
Louis Capitanchik <ljcapitanio@gmail.com>
2016-09-01 21:36:26 +0100

    New Style

--- webpack.config.development.js
+++ webpack.config.development.js
@@ -1,51 +1,51 @@
 /* eslint max-len: 0 */
-import webpack from 'webpack';
-import merge from 'webpack-merge';
-import baseConfig from './webpack.config.base';
+import webpack from 'webpack'
+import merge from 'webpack-merge'
+import baseConfig from './webpack.config.base'
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
 export default merge(baseConfig, {
-  debug: true,
+	debug: true,
 
-  devtool: 'cheap-module-eval-source-map',
+	devtool: 'cheap-module-eval-source-map',
 
-  entry: [
-    `webpack-hot-middleware/client?path=http://localhost:${port}/__webpack_hmr`,
-    './app/index'
-  ],
+	entry: [
+		`webpack-hot-middleware/client?path=http://localhost:${port}/__webpack_hmr`,
+		'./app/index',
+	],
 
-  output: {
-    publicPath: `http://localhost:${port}/dist/`
-  },
+	output: {
+		publicPath: `http://localhost:${port}/dist/`,
+	},
 
-  module: {
-    loaders: [
-      {
-        test: /\.global\.css$/,
-        loaders: [
-          'style-loader',
-          'css-loader?sourceMap'
-        ]
-      },
+	module: {
+		loaders: [
+			{
+				test: /\.global\.css$/,
+				loaders: [
+					'style-loader',
+					'css-loader?sourceMap',
+				],
+			},
 
-      {
-        test: /^((?!\.global).)*\.css$/,
-        loaders: [
-          'style-loader',
-          'css-loader?modules&sourceMap&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
-        ]
-      }
-    ]
-  },
+			{
+				test: /^((?!\.global).)*\.css$/,
+				loaders: [
+					'style-loader',
+					'css-loader?modules&sourceMap&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+				],
+			},
+		],
+	},
 
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development')
-    })
-  ],
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoErrorsPlugin(),
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify('development'),
+		}),
+	],
 
-  target: 'electron-renderer'
-});
+	target: 'electron-renderer',
+})
